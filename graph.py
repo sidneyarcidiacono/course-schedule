3b44846dee314c000487da484ed6613a510f9783
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-09 11:55:27 -0800

    Added initial course_prerequisites as JSON file, wrote basic graph to json function to print # of prerequisites for each course.

--- graph.py
+++ graph.py
@@ -8,18 +8,13 @@ class Graph:
 
     def __init__(self):
         """Initialize nodes and edges."""
-        self.nodes = None
+        self.nodes = []
         self.edges = []
 
     def add_vertex(self, id, value):
         """Add vertex to graph."""
         new_node = Vertex(id, value)
-        if self.nodes is not None and new_node.id in self.nodes:
-            return "This node already exists."
-        elif self.nodes is None:
-            self.nodes = {new_node.id: new_node.value}
-        elif new_node.id not in self.nodes and self.nodes is not None:
-            self.nodes[new_node.id] = new_node.value
+        self.nodes.append((new_node.id, new_node.value))
 
     def add_edge(self, start_node, end_node):
         """Add edge (connection) between two vertices."""
@@ -33,14 +28,3 @@ class Graph:
         """Print nodes and edges in graph."""
         print(f"Nodes: {self.nodes}")
         print(f"Edges: {self.edges}")
-
-
-graph = Graph()
-graph.add_vertex(1, "hello")
-graph.add_vertex(2, "sid")
-graph.add_vertex(3, "starlight")
-
-graph.add_edge(1, 2)
-graph.add_edge(2, 3)
-
-graph.print_graph()
---
+++ main.py
@@ -0,0 +1,19 @@
+"""Write our JSON file and use our graph to print our schedule."""
+import json
+from graph import Graph
+
+f = open("course_prerequisites.json")
+course_prereqs = json.load(f)
+
+
+def json_to_graph(json, graph):
+    """Take in JSON and create graph based on data."""
+    for d in json:
+        graph.add_vertex(d["name"], d["prerequisites"])
+    for v in graph.nodes:
+        print(f"{v[0]} has {len(v[1])} prerequisites")
+    return graph.print_graph()
+
+
+graph = Graph()
+json_to_graph(course_prereqs, graph)
