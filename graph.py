17357d34ca5d5cca75bbace10d41010919f3f317
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-09 12:14:16 -0800

    Wrote 'greedy' json to graph function, adding edges, needs optimization

--- graph.py
+++ graph.py
@@ -18,11 +18,18 @@ class Graph:
 
     def add_edge(self, start_node, end_node):
         """Add edge (connection) between two vertices."""
-        if not self.nodes[start_node] or not self.nodes[end_node]:
-            return "Start or end node does not exist."
         new_edge = Edge(start_node, end_node)
         edge_tuple = (new_edge.start_node, new_edge.end_node)
         self.edges.append(edge_tuple)
+
+    def find_node(self, node_to_find):
+        """Find node by name."""
+        i = 0
+        while i < len(self.nodes):
+            if self.nodes[i][0] == node_to_find:
+                return self.nodes[i][0]
+            else:
+                i += 1
 
     def print_graph(self):
         """Print nodes and edges in graph."""
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def json_to_graph(json, graph):
     """Take in JSON and create graph based on data."""
     for d in json:
         graph.add_vertex(d["name"], d["prerequisites"])
+        if d["prerequisites"]:
+            for prereq in d["prerequisites"]:
+                graph.add_edge(d, graph.find_node(prereq))
     for v in graph.nodes:
         print(f"{v[0]} has {len(v[1])} prerequisites")
     return graph.print_graph()
